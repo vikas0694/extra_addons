9ac97b719de66a6b48622b8569fa8ce7d954c7c6
vikas.0694 <vikas.0694@gmail.com>
2018-12-06 00:17:39 +0530

    models

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -1,15 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+class Course(models.Model):
+    _name = 'openacademy.course'
+    _description = "OpenAcademy Courses"
 
-# class openacademy(models.Model):
-#     _name = 'openacademy.openacademy'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+    name = fields.Char(string="Title", required=True)
+    description = fields.Text()
