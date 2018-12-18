e7d105a8f4b2e4a162e96341053430918ad319fd
vikas.0694 <vikas.0694@gmail.com>
2018-12-18 22:40:05 +0530

    manifest

--- openacademy/__manifest__.py
+++ openacademy/__manifest__.py
@@ -32,7 +32,7 @@
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
-        'views/templates.xml',
+        'views/openacademy.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
