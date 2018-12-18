3c80d2ec0913c9ba374c0a4c9573a4d955ceeb6a
vikas.0694 <vikas.0694@gmail.com>
2018-12-18 22:36:01 +0530

    manifest

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -1,13 +1,18 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+
 class Course(models.Model):
     _name = 'openacademy.course'
     _description = "OpenAcademy Courses"
 
     name = fields.Char(string="Title", required=True)
     description = fields.Text() 
-
+    
+    responsible_id = fields.Many2one('res.users',
+        ondelete='set null', string="Responsible", index=True)
+    session_ids = fields.One2many(
+        'openacademy.session', 'course_id', string="Sessions")
 
 class Session(models.Model):
     _name = 'openacademy.session'
@@ -18,6 +23,7 @@ class Session(models.Model):
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
 
-        instructor_id = fields.Many2one('res.partner', string="Instructor")
+    instructor_id = fields.Many2one('res.partner', string="Instructor")
     course_id = fields.Many2one('openacademy.course',
         ondelete='cascade', string="Course", required=True)
+    attendee_ids = fields.Many2many('res.partner', string="Attendees")
