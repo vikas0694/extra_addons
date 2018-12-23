d6b5f637fa28f68405ae46ff508fb123bb1793df
vikas.0694 <vikas.0694@gmail.com>
2018-12-23 15:03:34 +0530

    module

--- my_module/models/models.py
+++ my_module/models/models.py
@@ -2,6 +2,8 @@
 
 from odoo import models, fields, api
 
+
+
 # class my_module(models.Model):
 #     _name = 'my_module.my_module'
 
--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -27,3 +27,4 @@ class Session(models.Model):
     course_id = fields.Many2one('openacademy.course',
         ondelete='cascade', string="Course", required=True)
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
+    schedule = fields.Date()
